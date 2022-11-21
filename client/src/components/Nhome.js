63b889fc09d61c2b0c0c159e7fa172bee5229847
surajgoud36 <112563526+surajgoud36@users.noreply.github.com>
2022-11-20 19:59:58 -0500

    Update Nhome.js

--- client/src/components/Nhome.js
+++ client/src/components/Nhome.js
@@ -13,7 +13,7 @@ function Nhome() {
   const [test,setTest]=useState("yu");
   const useq=localStorage.getItem("token");
   useEffect(()=>{
-      Axios.get("http://50.17.117.140:9000/getUsers").then((response)=>{
+      Axios.get("http://3.89.93.76:9000/getUsers").then((response)=>{
         setListOfUsers(response.data);
         //setTest(response.data);
         //console.log("hi");
